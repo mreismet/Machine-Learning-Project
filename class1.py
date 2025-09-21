8506d87d198ebc654f61434eae927f6756315fa8
mreismet <mario.h.metzger@gmail.com>
2025-09-20 22:26:34 -0400

    Update class1.py

--- class1.py
+++ class1.py
@@ -1,13 +1,15 @@
 import pandas as pd;
 import json;
+import sklearn as sk;
 
 class my_class(object):
     with open('peopledata.json') as peopledata:
         data = json.load(peopledata)
         df = pd.DataFrame(data)
 
+      
 
-
+    from sk import decision_tree;
     print(df)
 
 

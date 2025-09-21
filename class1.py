2a0154577f1a2842395d4abf281bb086a0a1c0f2
mreismet <mario.h.metzger@gmail.com>
2025-09-20 22:37:12 -0400

    adding required libraries

--- class1.py
+++ class1.py
@@ -1,16 +1,18 @@
-import pandas as pd;
-import json;
-import sklearn as sk;
+import pandas as pd
+import json
+import sklearn as sk
 
 class my_class(object):
     with open('peopledata.json') as peopledata:
         data = json.load(peopledata)
         df = pd.DataFrame(data)
 
-      
-
-    from sk import decision_tree;
+    from sk import decision_tree
     print(df)
+
+
+
+
 
 
 
